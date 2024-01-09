835dc1d876b256dd61167c8d2ba77ef6429ffb5a
frontendlegend <aryngazy.khan@gmail.com>
2024-01-09 22:49:49 +0600

    refactor user model validation, reuse form partial

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,48 +1,50 @@
-class EmailValidator < ActiveModel::EachValidator
-    def validate_each(record, attribute, value)
-        unless URI::MailTo::EMAIL_REGEXP.match?(value)
-            record.errors.add attribute, (options[:message] || "is not an email")
-        end
-    end
-end
-
-PASSWORD_FORMAT = /\A
-  (?=.{8,})          # Must contain 8 or more characters
-  (?=.*\d)           # Must contain a digit
-  (?=.*[a-z])        # Must contain a lower case character
-  (?=.*[A-Z])        # Must contain an upper case character
-  (?=.*[[:^alnum:]]) # Must contain a symbol
-/x
-
-class PasswordValidator < ActiveModel::EachValidator
-    def validate_each(record, attribute, value)
-        unless PASSWORD_FORMAT.match?(value)
-            record.errors.add attribute, (
-                options[:message] ||
-                "Must contain 8 or more characters, " <<
-                "a digit, " <<
-                "a lower case character, " <<
-                "an upper case character " <<
-                "and a symbol"
-            )
-        end
-    end
-end
-
 class User < ApplicationRecord
     validates :username, 
-        presence: true, 
-        uniqueness: true, 
-        length: { minimum: 4 }
+        presence: { 
+            message: "Логин: обязательное поле" 
+        },
+        uniqueness: {
+            message: "Логин: занято" 
+        }, 
+        length: { 
+            minimum: 4,
+            message: "Логин: минимум 4 символа"
+        },
+        format: { 
+            with: /\A[a-zA-Z0-9]+\Z/, 
+            message: "Логин: без пробелов" 
+        }
 
     validates :email, 
-        presence: true, 
-        uniqueness: true, 
-        email: true
+        presence: { 
+            message: "Email: обязательное поле" 
+        },
+        uniqueness: {
+            message: "Email: занято" 
+        },
+        format: {
+            with: URI::MailTo::EMAIL_REGEXP,
+            message: "Email: это не email"
+        }
 
     validates :password, 
-        presence: true,
-        password: true
+        presence: { 
+            message: "Пароль: обязательное поле" 
+        },
+        format: {
+            with: /\A
+                      (?=.{8,})          # Must contain 8 or more characters
+                      (?=.*\d)           # Must contain a digit
+                      (?=.*[a-z])        # Must contain a lower case character
+                      (?=.*[A-Z])        # Must contain an upper case character
+                      (?=.*[[:^alnum:]]) # Must contain a symbol
+                  /x,
+            message: "Пароль: должен содержать 8 или более символов, в том числе: " <<
+                     "цифру, " <<
+                     "буквы нижнего " <<
+                     "и верхнего регистра, " <<
+                     "а также спец. символ"
+        }
 
     before_save :downcase_fields
 
